194be36e008b602f57232eb2692c497f5826347a
vdchien98 <vudangchien98@gmail.com>
2023-03-22 08:21:26 +0700

    firstcommit

---
+++ modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/constants/GuestbookPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.docs.guestbook.portlet.constants;
+
+/**
+ * @author User
+ */
+public class GuestbookPortletKeys {
+
+	public static final String GUESTBOOK = "com_liferay_docs_guestbook_portlet_GuestbookPortlet";
+	public static final String GUESTBOOK_ADMIN =
+			  "com_liferay_docs_guestbook_portlet_GuestbookAdminPortlet";
+}
